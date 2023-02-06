f025aa19989f647aea1795ab2f24c94f2c322913
PoryadinAndrey <cool.poryadin2014@yandex.ru>
2023-02-06 16:29:39 +0500

    first commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student stud = new Student();
+            stud.name = textBox1.Text;
+            stud.rost = (int)numericUpDown1.Value;
+            MessageBox.Show(string.Format($"Студент: {stud.name} \n Рост: {stud.rost} \n Вес: {stud.GetEat()}"));
+            stud.Eat(7);
+            double ed = 7.0;
+            MessageBox.Show(string.Format($"Студент: {stud.name} \n Cъел: {ed} кг еды \n \n Рост: {stud.rost} \n Вес: {stud.GetEat()}"));
+            stud.Eat(5);
+            ed = 5.0;
+            MessageBox.Show(string.Format($"Студент: {stud.name} \n Cъел: {ed} кг еды \n \n Рост: {stud.rost} \n Вес: {stud.GetEat()}"));
+            stud.Eat(6);
+            ed = 6.0;
+            MessageBox.Show(string.Format($"Студент: {stud.name} \n Cъел: {ed} кг еды \n \n Рост: {stud.rost} \n Вес: {stud.GetEat()}"));
+        }
+    }
+}
---
+++ WindowsFormsApp1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Student
+    {
+        public string name;
+        public int rost;
+        private double ves = 50;
+        public void Eat(double ed)
+        {
+            if (ed > 5 && ed < 10)
+            {
+                rost -= 1;
+                ves += (((ed * 1000 - 1600) / 1000) / 1000) * 0.7;
+            }
+            else if (ed > 10)
+            {
+                rost -= 2;
+                ves += ((ed * 1000 - 1800) / 1000) * 0.5;
+            }
+            else
+                ves += (ed * 1000 - 1600) / 1000;
+        }
+        public double GetEat()
+        {
+            return ves;
+        }
+    }
+}
